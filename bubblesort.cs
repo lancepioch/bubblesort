7ee7c56be674c2514de05851ec31a1c21b6a329d
Lance Pioch <lance@lancepioch.com>
2013-01-26 21:08:26 -0500

    Added C# example

---
+++ bubblesort.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Bubblesort {
+    /// <summary>
+    /// Simple utility type. 
+    /// </summary>
+    public class Bubblesort {
+        /// <summary>
+        /// Print out select environment information
+        /// </summary>
+        public static void Main() {
+            float[] array = new float[] {1337, 1, -465, 3.141592653589793f, 789, 69, 789, -132, 3.141592653589793f, 465, 789, 0, 27};
+            array = bubblesort(array);
+            for (int i=0; i < array.Length; Console.Write(" " + array[i] + " "), i++);
+            // Console.WriteLine("Location of this program: {0}", Environment.CurrentDirectory);
+        }
+
+        public static float[] bubblesort(float[] array) {
+            bool swapping = false;
+            float temp;
+            while (!swapping) {
+                swapping = true;
+                for (int i = 0; i < array.Length - 1; i++) {
+                    if (array[i] > array[i+1]) {
+                        temp = array[i+1];
+                        array[i+1] = array[i];
+                        array[i] = temp;
+                        swapping = false;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
